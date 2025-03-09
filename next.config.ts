9856abebff99314d9981c997395c502f539de27d
hyeongyu An <hyeongyu2656@gmail.com>
2025-03-09 22:20:44 +0900

    chore: assetPrefix 값 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const assetPrefix = process.env.NODE_ENV === 'production' ? 'https://oh-my-wedding.github.io/invitation/' : '';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  assetPrefix,
 };
 
 export default nextConfig;
