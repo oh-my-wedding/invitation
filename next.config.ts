49f0d5d504828a19b77594b89d500deb743f35dc
hyeongyu An <hyeongyu2656@gmail.com>
2025-03-09 22:34:08 +0900

    chore: basePath 값 추가

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const assetPrefix = process.env.NODE_ENV === 'production' ? 'https://oh-my-wedding.github.io/invitation/' : '';
+const basePath = '/invitation';
 
 const nextConfig: NextConfig = {
   output: 'export',
   assetPrefix,
+  basePath,
 };
 
 export default nextConfig;
