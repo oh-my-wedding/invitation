806920506a56492bae5c67445995b12057918de6
hyeongyu An <hyeongyu2656@gmail.com>
2025-03-10 00:40:52 +0900

    chore: 불필요한 요소 제거

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -2,8 +2,6 @@ export const Comments = () => {
   return (
     <div>
       <h2>축하 인사 남기기</h2>
-      <div className="flex flex-col w-full max-w-md h-[600px] bg-white rounded-xl overflow-hidden shadow-lg">
-      </div>
     </div>
   )
 };
