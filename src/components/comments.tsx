6f2a2f55dbf9896c755b85e379134f034aa6b665
hyeongyu An <hyeongyu2656@gmail.com>
2025-03-09 22:16:52 +0900

    update: 사용하지 않는 컴포넌트 제거

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -1,11 +1,8 @@
-import { KakaoTalk } from "@/components/kakao-talk";
-
 export const Comments = () => {
   return (
     <div>
       <h2>축하 인사 남기기</h2>
       <div className="flex flex-col w-full max-w-md h-[600px] bg-white rounded-xl overflow-hidden shadow-lg">
-        <KakaoTalk />
       </div>
     </div>
   )
