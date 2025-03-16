334ca9589e64e47e91fbdbac7a804f6ec713e305
hyeongyu An <hyeongyu2656@gmail.com>
2025-03-17 00:04:51 +0900

    chore: 폰트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,30 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+
 const suit = localFont({
-  src: './font/SuitRegular.woff2',
+  src: [
+    {
+      path: './font/SuitThin.woff2',
+      weight: '100',
+    },
+    {
+      path: './font/SuitExtraLight.woff2',
+      weight: '200',
+    },
+    {
+      path: './font/SuitLight.woff2',
+      weight: '300',
+    },
+    {
+      path: './font/SuitRegular.woff2',
+      weight: '400',
+    },
+    {
+      path: './font/SuitMedium.woff2',
+      weight: '500',
+    }
+  ],
   display: 'swap',
   variable: '--font-suit',
 });
