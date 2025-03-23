fa523a433ab9be94b584c67f95b01dca0621c3f2
hyeongyu An <hyeongyu2656@gmail.com>
2025-03-23 20:05:33 +0900

    update: 모달 스타일 변경

--- src/components/write-comment-modal.tsx
+++ src/components/write-comment-modal.tsx
@@ -41,13 +41,13 @@ export const WriteCommentModal = ({ isShow, onSubmit, onClose }: WriteCommentMod
         {/* <!-- Modal content --> */}
         <div className="relative bg-white rounded-lg shadow-sm">
             {/* <!-- Modal header --> */}
-            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
+            <div className="flex items-center justify-between p-4 rounded-t">
               <h3 className="text-lg font-semibold text-gray-900">
                 축하 메세지 작성하기
               </h3>
               <button
                 type="button"
-                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+                className="text-gray-400 bg-transparent rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
                 onClick={onClose}
               >
                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -97,7 +97,7 @@ export const WriteCommentModal = ({ isShow, onSubmit, onClose }: WriteCommentMod
               </div>
               <button
                 type="submit"
-                className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                className="text-white inline-flex items-center bg-[#afc18b] font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
                 작성 완료
               </button>
